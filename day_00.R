09a1ea8f32c487a931ff6062515284d0e338740f
Margus Lehtme <marguslt@ut.ee>
2022-12-05 23:45:04 +0200

    merge

--- day_00.R
+++ day_00.R
@@ -1,7 +1,11 @@
+# -------------------------------------------------------------------------
 library(dplyr, warn.conflicts = F)
 library(readr)
-
+library(testthat)
+library(tidyverse)
 source("helpers/aoc.R")
+
+test <- list(t = get_sample())
 df <- read_table(get_aoc(), col_names = F)
 
 # test data ---------------------------------------------------------------
@@ -12,6 +16,7 @@ p2 = c(),
 a1 = 0, 
 a2 = 0)
 
+df_ <- read_table(test$t, col_names = F)
 
 # part 1 ------------------------------------------------------------------
 part_1 <- function(d = df){
@@ -24,18 +29,19 @@ part_2 <- function(d = df){
 }
 
 # test --------------------------------------------------------------------
-testthat::test_that("AOC", {
+test_that("AOC", {
   df_ <- read_table(test$t, col_names = F)
-  # testthat::expect_equal(f1_(df_$X1, df_$X2),test$p1)
-  # testthat::expect_equal(f2_(df_$X1, df_$X2),test$p2)
+  # expect_equal(f1_(df_$X1, df_$X2),test$p1)
+  # expect_equal(f2_(df_$X1, df_$X2),test$p2)
   
-  testthat::expect_equal(part_1(df_),test$a1)
-  testthat::expect_equal(part_2(df_),test$a2)
+  expect_equal(
+    part_1(!!df_),
+    !!test$a1 )
+  expect_equal(
+    part_2(!!df_),
+    !!test$a2 )
 })
 
 # -------------------------------------------------------------------------
 part_1()
 part_2()
-
-
-
--- helpers/aoc.R
+++ helpers/aoc.R
@@ -1,15 +1,15 @@
 library(httr)
 # .aoc_session contains session cookie
 
-get_aoc <- function(year=NULL, day=NULL){
+get_aoc <- function(year_=NULL, day_=NULL){
   AOC_COOKIES <- c(session = readr::read_file(".aoc_session"))
-  if (is.null(year) || is.null(day)){
-    date <-strsplit(format(Sys.Date(), "%Y,%d"), ",") |> unlist()
-    year <- date[1]    
-    day <- date[2]    
+  if (is.null(year_) || is.null(day_)){
+    date_ <-strsplit(format(Sys.Date(), "%Y,%d"), ",") |> unlist()
+    year_ <- date_[1]    
+    day_ <- date_[2]    
   }
-  filename <- paste0("in/input_", day)
-  url <- paste0("https://adventofcode.com/",year,"/day/",as.numeric(day),"/input")
+  filename <- paste0("in/input_", day_)
+  url <- paste0("https://adventofcode.com/",year_,"/day/",as.numeric(day_),"/input")
   message(paste(url, ">>", filename))
   if (!file.exists(filename)){
     resp <- GET(url, set_cookies(AOC_COOKIES))
@@ -20,9 +20,39 @@ get_aoc <- function(year=NULL, day=NULL){
     message("Sample 1:\n")
     read_file(paste0(filename,"_sample_1")) %>% cat()
   }
+<<<<<<< HEAD
   message("top 25 lines:\n")
   read_lines(filename, n_max = 25) %>% paste(collapse = "\n") %>% cat()
+=======
+  message("\n# top 25 lines ------------------------------------------------------------\n")
+  read_lines(filename, n_max = 25) %>% paste(collapse = "\n") %>% cat()
+  message("\n# -------------------------------------------------------------------------\n")
+>>>>>>> e8257fb (template & flow changes)
 
   return(filename)
 }
 
+<<<<<<< HEAD
+=======
+get_sample <- function(year_=NULL, day_=NULL){
+  if (is.null(year_) || is.null(day_)){
+    date_ <-strsplit(format(Sys.Date(), "%Y,%d"), ",") |> unlist()
+    year_ <- date_[1]    
+    day_ <- date_[2]    
+  }
+  filename <- paste0("in/sample_1_", day_)
+  url <- paste0("https://adventofcode.com/",year_,"/day/",as.numeric(day_) )
+  message(cli::style_hyperlink(url, url))
+  if (!file.exists(filename)){
+    aoc <- read_html(url)
+    aoc %>% html_element("article.day-desc > pre:first-of-type > code") %>% html_text() %>% write_file(filename)
+  }
+  s <-read_file(filename) 
+  message("\n# Part 1 sample -----------------------------------------------------------\n")
+  s %>% cat()
+  message("\n# -------------------------------------------------------------------------\n")
+  return(s)
+}
+
+
+>>>>>>> e8257fb (template & flow changes)
