35839fa5023b4ce470fecb90f33b5f8c599593b1
Margus Lehtme <marguslt@ut.ee>
2022-12-05 23:41:36 +0200

    template & flow changes

--- helpers/aoc.R
+++ helpers/aoc.R
@@ -14,6 +14,15 @@ get_aoc <- function(year=NULL, day=NULL){
   if (!file.exists(filename)){
     resp <- GET(url, set_cookies(AOC_COOKIES))
     write(content(resp, as = "text"), file = filename)
+
+    aoc <- read_html(paste0("https://adventofcode.com/",year,"/day/",as.numeric(day))) 
+    aoc %>% html_element("article.day-desc > pre:first-of-type > code") %>% html_text() %>% write_file(paste0(filename,"_sample_1"))
+    message("Sample 1:\n")
+    read_file(paste0(filename,"_sample_1")) %>% cat()
   }
+  message("top 25 lines:\n")
+  read_lines(filename, n_max = 25) %>% paste(collapse = "\n") %>% cat()
+
   return(filename)
 }
+
